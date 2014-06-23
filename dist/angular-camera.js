9d110586ee12491befea98e5856de36b1e0d9f2d
William Boman <william@redwill.se>
2014-06-23 13:13:38 +0200

    Added error callback argument to `webkitGetUserMedia()` in `scope.disableCamera()`. Removed `grunt-cli` from package.json. Added compass(sass) styles. Fixed some css.

--- dist/angular-camera.js
+++ dist/angular-camera.js
@@ -6,8 +6,8 @@
     function ($timeout, $sce) {
       return {
         require: 'ngModel',
-        template: '<div class="ng-camera clearfix">        <p ng-hide="isLoaded">Loading Camera...</p>        <p ng-show="noCamera">Couldn\'t find a camera to use</p>        <div class="ng-camera-stack" ng-hide="!isLoaded">          <div class="ng-camera-countdown" ng-show="activeCountdown">            <p class="tick">{{countdownText}}</p>          </div>          <img class="ng-camera-overlay" ng-hide="!overlaySrc" ng-src="{{overlaySrc}}" width="{{width}}" height="{{height}}">          <video id="ng-camera-feed" autoplay width="{{width}}" height="{{height}}" src="{{videoStream}}">Install Browser\'s latest version</video>          <canvas id="ng-photo-canvas" width="{{width}}" height="{{height}}" style="display:none;"></canvas>        </div>        <div class="ng-camera-controls" ng-hide="hideUI">          <button class="btn ng-camera-take-btn" ng-click="takePicture()">Take Picture</button>        </div>      </div>',
-        replace: false,
+        template: '<div class="ng-camera clearfix" style="width: {{width}}px; height: {{height}}px">        <p ng-hide="isLoaded">Loading Camera...</p>        <p ng-show="noCamera">Couldn\'t find a camera to use</p>        <div class="ng-camera-stack" ng-hide="!isLoaded">          <div class="ng-camera-countdown" ng-show="activeCountdown">            <p class="tick">{{countdownText}}</p>          </div>          <img class="ng-camera-overlay" ng-hide="!overlaySrc" ng-src="{{overlaySrc}}" width="{{width}}" height="{{height}}">          <video id="ng-camera-feed" autoplay src="{{videoStream}}">Install Browser\'s latest version</video>          <canvas id="ng-photo-canvas" width="{{width}}" height="{{height}}" style="display:none;"></canvas>        </div>        <div class="ng-camera-controls" ng-hide="hideUI">          <button class="btn btn-primary ng-camera-take-btn" ng-click="takePicture()">Take Picture</button>        </div>      </div>',
+        replace: true,
         transclude: true,
         restrict: 'E',
         scope: {
@@ -30,6 +30,9 @@
               scope.stream.stop();
             }
           });
+          /**
+        * @description Set mediastream source and notify camera
+        */
           scope.enableCamera = function () {
             return navigator.getUserMedia({
               audio: false,
@@ -47,6 +50,9 @@
               });
             });
           };
+          /**
+        * @description Disable mediastream source and notify camera
+        */
           scope.disableCamera = function () {
             return navigator.getUserMedia({
               audio: false,
@@ -55,8 +61,12 @@
               return scope.$apply(function () {
                 return scope.videoStream = '';
               });
+            }, function () {
             });
           };
+          /**
+        * @description Capture current state of video stream as photo
+        */
           scope.takePicture = function () {
             var canvas, context, countdownTick, countdownTime;
             canvas = window.document.getElementById('ng-photo-canvas');
@@ -109,6 +119,10 @@
             }
             return false;
           };
+          /**
+        * @description Add overlay frame to canvas render
+        * @param {Object} context Reference to target canvas context
+        */
           scope.addFrame = function (context, url, callback) {
             var overlay;
             if (callback == null) {
@@ -124,11 +138,18 @@
             overlay.crossOrigin = '';
             return overlay.src = url;
           };
+          /**
+        * @description Keeps a packaged version of media ready
+        * @param {Base64} newVal Prefix-stripped Base64 of of canvas image
+        */
           scope.$watch('media', function (newVal) {
             if (newVal != null) {
               return scope.packagedMedia = scope.media.replace(/^data:image\/\w+;base64,/, '');
             }
           });
+          /**
+        * @description Preloader for overlay image
+        */
           scope.$watch('overlaySrc', function (newVal, oldVal) {
             var preloader;
             if (scope.overlaySrc != null) {
@@ -145,6 +166,9 @@
               return scope.isLoaded = true;
             }
           });
+          /**
+        * @description Watch for when to turn on/off camera feed
+        */
           scope.$watch('enabled', function (newVal, oldVal) {
             if (newVal) {
               if (!oldVal) {
@@ -156,6 +180,10 @@
               }
             }
           });
+          /**
+        * @description Check format type of camera.
+        * @todo Future support for different media types (GIF, Video)
+        */
           return scope.$watch('type', function () {
             switch (scope.type) {
             case 'photo':
