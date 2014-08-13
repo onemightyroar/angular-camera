12d903ec5167d22edb6e4cf9027b32f03158ee59
Zach Dunn <zach@onemightyroar.com>
2014-08-12 22:29:59 -0400

    Bumping version to 0.1.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,53 @@
 'use strict';
 
 module.exports = function(grunt) {
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-    grunt.initConfig({
-
-        clean: {
-            dist: {
-                files: [{
-                    dot: true,
-                    src: ['.tmp', 'dist/*', '!dist/.git*']
-                }]
-            }
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.initConfig({
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: ['.tmp', 'dist/*', '!dist/.git*']
+        }]
+      }
+    },
+    coffee: {
+      dev: {
+        options: {
+          sourceMap: false
         },
-        coffee: {
-            dev: {
-                options: {
-                    sourceMap: false
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/directives',
-                    src: '{,*/}*.coffee',
-                    dest: 'src/directives',
-                    ext: '.js'
-                }]
-            }
-        },
-        concat: {
-            src: {
-                src: ['src/directives/angular-camera.js'],
-                dest: 'dist/angular-camera.js'
-            }
-        },
-        ngmin: {
-            src: {
-                src: '<%= concat.src.src %>',
-                dest: '<%= concat.src.dest %>'
-            }
-        },
-        uglify: {
-            src: {
-                files: {
-                    'dist/angular-camera.min.js': '<%= concat.src.dest %>'
-                }
-            }
+        files: [{
+          expand: true,
+          cwd: 'src/directives',
+          src: '{,*/}*.coffee',
+          dest: 'src/directives',
+          ext: '.js'
+        }]
+      }
+    },
+    concat: {
+      src: {
+        src: ['src/directives/angular-camera.js'],
+        dest: 'dist/angular-camera.js'
+      }
+    },
+    ngmin: {
+      src: {
+        src: '<%= concat.src.src %>',
+        dest: '<%= concat.src.dest %>'
+      }
+    },
+    uglify: {
+      src: {
+        files: {
+          'dist/angular-camera.min.js': '<%= concat.src.dest %>'
         }
-    });
+      }
+    }
+  });
 
-
-    grunt.registerTask('build', ['dev', 'clean:dist', 'concat', 'ngmin', 'uglify']);
-
-    grunt.registerTask('dev', ['coffee:dev']);
-
-    grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['dev', 'clean:dist', 'concat', 'ngmin', 'uglify']);
+  grunt.registerTask('dev', ['coffee:dev']);
+  grunt.registerTask('default', ['build']);
 };
